4d2f6775c723ae474d9ed9b309d1a249bd6e179d
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-11 11:59:05 +0300

    Edit: add map controllers

--- ManagementSystem.API/Program.cs
+++ ManagementSystem.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<EmployeeService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -16,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
