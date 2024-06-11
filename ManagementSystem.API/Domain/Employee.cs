3aa0733b2cfb6d9305d968647f7700bffa84910d
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-11 10:50:24 +0300

    Update: add employee model

---
+++ ManagementSystem.API/Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManagementSystem.API.Domain;
+
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "First name is obligatory")]
+    [MaxLength(50, ErrorMessage = "{0} can have max of {1} characters")]
+    public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Last name is obligatory")]
+    [MaxLength(50, ErrorMessage = "{0} can have max of {1} characters")]
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Position { get; set; }
+    public double AnnualSalary { get; set; }
+}
