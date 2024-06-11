74a167e3d0d976cda9750d0b1737c1ce6f845f49
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-11 11:58:45 +0300

    Edit: change property in employee model

--- ManagementSystem.API/Domain/Employee.cs
+++ ManagementSystem.API/Domain/Employee.cs
@@ -15,7 +15,7 @@ public class Employee
     [Required(ErrorMessage = "Last name is obligatory")]
     [MaxLength(50, ErrorMessage = "{0} can have max of {1} characters")]
     public string LastName { get; set; }
-    public DateTime BirthDate { get; set; }
+    public int Age { get; set; }
     public string Position { get; set; }
     public double AnnualSalary { get; set; }
 }
