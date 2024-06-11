418abf5cc7ca65160f5b69d757bf28d064dd65cb
Vladislav Lashkin <lashkin2004@gmail.com>
2024-06-11 11:40:29 +0300

    Update: edit employee service

--- ManagementSystem.API/Services/EmployeeService.cs
+++ ManagementSystem.API/Services/EmployeeService.cs
@@ -4,54 +4,57 @@ namespace ManagementSystem.API.Services;
 
 public class EmployeeService
 {
-    private List<Employee> _employees = new List<Employee>();
+    private readonly List<Employee> _employees = new List<Employee>();
 
     public IEnumerable<Employee> GetEmployees()
     {
         return _employees;
     }
 
-    public Employee GetEmployee(Guid id)
+    public Employee? GetEmployee(Guid id)
     {
         int index = _employees.FindIndex(e => e.Id == id);
         if (index < 0)
         {
-            throw new Exception($"Employee with {id} id was not found");
+            return null;
         }
 
         return _employees[index];
     }
 
-    public void PostEmployee(Employee employee)
+    public Guid? PostEmployee(Employee employee)
     {
         int index = _employees.FindIndex(e => e.Id == employee.Id);
         if (index >= 0)
         {
-            throw new Exception($"Employee with {employee.Id} already exists");
+            return null;
         }
         
         _employees.Add(employee);
+        return employee.Id;
     }
 
-    public void PutEmployee(Guid id, Employee employee)
+    public Guid? PutEmployee(Guid id, Employee employee)
     {
         int index = _employees.FindIndex(e => e.Id == id);
         if (index < 0)
         {
-            throw new Exception($"Employee with {id} id was not found");
+            return null;
         }
 
         _employees[index] = employee;
+        return id;
     }
 
-    public void DeleteEmployee(Guid id)
+    public Guid? DeleteEmployee(Guid id)
     {
         int index = _employees.FindIndex(e => e.Id == id);
         if (index < 0)
         {
-            throw new Exception($"Employee with {id} id was not found");
+            return null;
         }
         
         _employees.RemoveAt(index);
+        return id;
     }
 }
